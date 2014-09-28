bff16a9bce9854b7cfa8ff8c02fa590b793ac4dc
Diego Murillo <ddmurillo@gmail.com>
2014-09-27 20:45:20 -0500

    Agrega clases para reducir el lado derecho y para quitar atributos extraños

---
+++ AlgoritmoBernstein/src/com/anstek/negocio/AtributosExtranios.java
@@ -0,0 +1,123 @@
+/**
+ * 
+ */
+package com.anstek.negocio;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import com.anstek.clases.Atributo;
+import com.anstek.clases.DependenciaFuncional;
+import com.anstek.utiles.Cierre;
+
+/**
+ * @author ddmurillo
+ *
+ */
+public class AtributosExtranios {
+	/**
+	 * Elimina atributos extraños de las dependencias funcionales (solo aplica para los implicantes com mas de un atributo)
+	 * @param dependencias Dependencias funcionales
+	 * @return Array con las dependencias funcionales sin atributos extraños
+	 */
+	public static DependenciaFuncional[] LimpiaAtributosExtranios(DependenciaFuncional[] dependencias) {
+		List<DependenciaFuncional> newDependencias = new ArrayList<DependenciaFuncional>();
+
+		for (int i = 0; i < dependencias.length; i++) {
+			// Se simplifican los implicantes con mas de un atributo
+			if (dependencias[i].getImplicante().size() > 1) {
+				//
+				HashSet<String> dfExtr = new HashSet<String>();
+				for (String v : dependencias[i].getImplicante()) {
+					// Quita el implicante actual
+					HashSet<String> temp = new HashSet<String>(dependencias[i].getImplicante());
+					temp.remove(v);
+					
+					// Array temporal de atributos de implicantes sobrantes
+					Atributo[] arrAtt = new Atributo[temp.size()];
+					int flag = 0;
+					for (String t : temp) {
+						arrAtt[flag] = new Atributo(t.charAt(0), t);
+						flag++;
+					}						
+					
+					// Realiza el cierre de los demas implicantes
+					HashSet<String> strange = Cierre.HacerCierre(arrAtt, dependencias);
+					
+					//Es extraño
+					if (strange.contains(v)) {
+						dfExtr.add(v);
+					}
+				}
+				
+				// La dependencia tiene atributos extraños
+				if (dfExtr.size() > 0) {
+					HashSet<String> implicantesLimpios = new HashSet<String>(dependencias[i].getImplicante());
+					for (String s : dfExtr) {
+						implicantesLimpios.remove(s);
+					}
+					// Nueva dependencia
+					DependenciaFuncional df = new DependenciaFuncional(implicantesLimpios, dependencias[i].getImplicado());
+					newDependencias.add(df);
+				}
+				
+			} else if (dependencias[i].getImplicante().size() == 1) {
+				DependenciaFuncional df = new DependenciaFuncional(dependencias[i].getImplicante(),dependencias[i].getImplicado());
+				newDependencias.add(df);
+			}
+		}
+
+		// Convierte en array
+		DependenciaFuncional[] arrDependencias = new DependenciaFuncional[newDependencias.size()];
+		newDependencias.toArray(arrDependencias);
+
+		return arrDependencias;
+	}
+	
+	public static void main (String[] args){
+		//Atributo a1 = new Atributo('a', "A");
+		//Atributo a2 = new Atributo('b', "B");
+		//Atributo a3 = new Atributo('c', "C");
+		//Atributo a4 = new Atributo('d', "D");
+		
+		HashSet<String> hs1 = new HashSet<String>();
+		hs1.add("a");
+		hs1.add("d");
+		
+		HashSet<String> hs2 = new HashSet<String>();
+		hs2.add("b");
+		
+		HashSet<String> hs3 = new HashSet<String>();
+		hs3.add("c");
+		
+		HashSet<String> hs4 = new HashSet<String>();
+		hs4.add("d");		
+		
+		HashSet<String> hs5 = new HashSet<String>();
+		hs5.add("a");
+		hs5.add("b");
+		
+		HashSet<String> hs6 = new HashSet<String>();
+		hs6.add("e");
+		
+		HashSet<String> hs7 = new HashSet<String>();
+		hs7.add("a");
+		hs7.add("c");
+		
+		HashSet<String> hs8 = new HashSet<String>();
+		hs8.add("f");
+		
+		DependenciaFuncional dep1 = new DependenciaFuncional(hs1, hs2);
+		DependenciaFuncional dep2 = new DependenciaFuncional(hs2, hs3);
+		DependenciaFuncional dep3 = new DependenciaFuncional(hs3, hs4);
+		DependenciaFuncional dep4 = new DependenciaFuncional(hs5, hs6);
+		DependenciaFuncional dep5 = new DependenciaFuncional(hs7, hs8);
+		
+		DependenciaFuncional[] res = AtributosExtranios.LimpiaAtributosExtranios(new DependenciaFuncional[]{dep1,dep2,dep3,dep4,dep5});
+		
+		for (int i = 0; i < res.length; i++) {
+			System.out.println(res[i].getImplicante() + " -> " + res[i].getImplicado());
+		}
+	}
+}
---
+++ AlgoritmoBernstein/src/com/anstek/negocio/LDS.java
@@ -0,0 +1,103 @@
+/**
+ * 
+ */
+package com.anstek.negocio;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import com.anstek.clases.DependenciaFuncional;
+
+/**
+ * @author ddmurillo
+ *
+ */
+public class LDS {
+	/**
+	 * Dado un conjunto de dependencias funcionales retorna un nuevo conjunto de DFs simplificando los implicados (lado derecho)
+	 * @param dependencias
+	 * @return
+	 */
+	public static DependenciaFuncional[] LadoDerechoSimple(DependenciaFuncional[] dependencias){
+		List<DependenciaFuncional> newDependencias = new ArrayList<DependenciaFuncional>();
+		
+		for (int i = 0; i < dependencias.length; i++) {
+			// Se simplifican los implicados con mas de un atributo
+			if (dependencias[i].getImplicado().size() > 1) {
+				for (String v : dependencias[i].getImplicado()) {
+					HashSet<String> h1 = new HashSet<String>();
+					h1.add(v);
+					//Nueva dependencia
+					DependenciaFuncional df = new DependenciaFuncional(dependencias[i].getImplicante(), h1);
+					newDependencias.add(df);
+				}				
+			}
+			else if (dependencias[i].getImplicado().size() == 1) {
+				DependenciaFuncional df = new DependenciaFuncional(dependencias[i].getImplicante(), dependencias[i].getImplicado());
+				newDependencias.add(df);
+			}
+		}
+		
+		// Convierte en array
+		DependenciaFuncional[] arrDependencias = new DependenciaFuncional[newDependencias.size()];
+		newDependencias.toArray(arrDependencias);
+		
+		return arrDependencias;
+	}
+	
+	/**
+	 * Main de prueba
+	 * @param args
+	 */
+	public static void main (String[] args){
+		//Atributo a1 = new Atributo('a', "A");
+		//Atributo a2 = new Atributo('b', "B");
+		//Atributo a3 = new Atributo('c', "C");
+		//Atributo a4 = new Atributo('d', "D");
+		
+		HashSet<String> hs1 = new HashSet<String>();
+		hs1.add("a");
+		hs1.add("b");
+		
+		HashSet<String> hs2 = new HashSet<String>();
+		hs2.add("e");
+		
+		HashSet<String> hs3 = new HashSet<String>();
+		hs3.add("a");
+		hs3.add("g");
+		
+		HashSet<String> hs4 = new HashSet<String>();
+		hs4.add("j");		
+		
+		HashSet<String> hs5 = new HashSet<String>();
+		hs5.add("b");
+		hs5.add("e");
+		
+		HashSet<String> hs6 = new HashSet<String>();
+		hs6.add("i");
+		
+		HashSet<String> hs7 = new HashSet<String>();
+		hs7.add("g");
+		
+		HashSet<String> hs8 = new HashSet<String>();
+		hs8.add("g");
+		hs8.add("i");
+		
+		HashSet<String> hs9 = new HashSet<String>();
+		hs9.add("h");
+		
+		DependenciaFuncional dep1 = new DependenciaFuncional(hs1, hs3);
+		DependenciaFuncional dep2 = new DependenciaFuncional(hs3, hs4);
+		DependenciaFuncional dep3 = new DependenciaFuncional(hs4, hs7);
+		DependenciaFuncional dep4 = new DependenciaFuncional(hs5, hs8);
+		//DependenciaFuncional dep5 = new DependenciaFuncional(hs8, hs9);
+		
+		//HashSet<String> res = Cierre.HacerCierre(new Atributo[]{a1, a2},new DependenciaFuncional[]{dep1,dep2,dep3,dep4,dep5});
+		DependenciaFuncional[] res = LDS.LadoDerechoSimple(new DependenciaFuncional[]{dep1,dep2,dep3,dep4});
+		
+		for (int i = 0; i < res.length; i++) {
+			System.out.println(res[i].getImplicante() + " -> " + res[i].getImplicado());
+		}
+	}
+}
